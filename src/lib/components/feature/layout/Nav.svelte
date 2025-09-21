8f88c0b91c798a57686d52cfd6111e618b77453d
LeeJinyoung218 <amy021899@gmail.com>
2025-09-21 16:30:58 +0900

    feat: 레이아웃 설정

---
+++ src/lib/components/feature/layout/Nav.svelte
@@ -0,0 +1,3 @@
+<div class="w-full h-fit Content">
+    Nav
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import Nav from "$lib/components/feature/layout/Nav.svelte";
+    import Intro from "$lib/components/feature/layout/Intro.svelte";
+    import EduCert from "$lib/components/feature/layout/EduCert.svelte";
+    import Projects from "$lib/components/feature/layout/Projects.svelte";
+    import Skills from "$lib/components/feature/layout/Skills.svelte";
+</script>
+
+<div class="bg-background flex">
+    <main class="flex-1 flex flex-col items-center justify-start min-h-screen gap-16 p-16">
+        <Intro />
+        <div class="h-[1px] w-1 bg-light-gray"></div>
+        <EduCert />
+        <div class="h-[1px] w-1 bg-light-gray"></div>
+        <Skills />
+        <div class="h-[1px] w-1 bg-light-gray"></div>
+        <Projects />
+        <div class="h-[1px] w-1 bg-light-gray"></div>
+    </main>
+    <aside class="w-1/4 min-h-screen">
+        <Nav />
+    </aside>
+</div>
